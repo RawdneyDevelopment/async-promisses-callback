f878379af1b285e007ad9d78cf2d019ef7c91026
Rawdney Mendes <mendesrawdney@gmail.com>
2022-10-11 03:51:58 -0300

    ediferença entre async/await e promessas

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-console.log(1);
-console.log(2);
-console.log(3);
-/* o javascript executa o código por meio de filas
-o código é executado de forma sequencia.
-*/
+const request = require('request');
+const gitHubApi = 'https://github.com/RawdneyGoncalves?tab=repositories'
 
-setTimeout(()=>{
-    console.log('setTimeOut data')
-}, 1);
-
-const promise = new Promise(resolve => {
-    resolve('promise data')
-});
-
-
-for(let i= 1; i <= 1000, i++){
-    console.log(i)
-}
-
-/* no event loop ele irá pula as filas que tem pendencia
-assim deixando pra pra realizar na proxima fila.
-*/
+request(gitHubApi, { 'User-Agent': 'node.js' }, (err, res, body) => {
+    console.log(JSON.parse(body))
+})
